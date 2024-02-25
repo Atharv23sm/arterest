0cdfcb0268ab3a785f7cf23c8c2b819ebdb86d2e
Atharv Mahabal <122306441+Atharv23sm@users.noreply.github.com>
2024-02-26 02:35:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
-app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', indexRouter);
 app.use('/', homeRouter);
